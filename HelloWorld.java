b522543e4bc75589066dca4ec58f9fc1c0acf90a
HemaGRepo <hema.g@capgemini.com>
2018-06-11 15:29:56 +0530

    v2.0

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++)
-		System.out.println("Welcome to GIT - Jenkins.."+i);
+		for(int i=1;i<=10;i++)
+		System.out.println("The pipeline rune.."+i);
 	}
 
 }
